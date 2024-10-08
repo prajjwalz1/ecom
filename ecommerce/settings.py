4d2447fbbf49bab3ff8a0be9a3caed6b018d8439
prajjwalz1 <acharyaprajjwol152@gmail.com>
2024-10-09 01:20:21 +0545

    update the setting.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -43,12 +43,12 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'debug_toolbar',
-    'Order'
-
+    'corsheaders'
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = '/static/'  
+STATIC_URL = '/static/'
 
 STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
@@ -192,3 +192,4 @@ CKEDITOR_CONFIGS = {
         'filebrowserImageUploadUrl': '/ckeditor/upload/',  # URL for image uploads
     },
 }
+CORS_ALLOW_ALL_ORIGINS = True

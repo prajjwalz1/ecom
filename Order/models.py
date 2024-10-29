42a7dc66392480217a6dc7e2bf995bf56c8c275f
prajjwal <prajjwal.acharya@mypay.com.np>
2024-10-29 11:01:52 +0545

    update the varaint naming function

--- Order/models.py
+++ Order/models.py
@@ -4,8 +4,8 @@ from product.models import Product
 # Create your models here.
 
 class OrderItem(CustomizedModel):
-    order=models.ForeignKey('Order',on_delete=models.SET_DEFAULT,default="deleted_order")
-    product=models.ForeignKey(Product,on_delete=models.SET_DEFAULT,default="deleted_product")
+    order=models.ForeignKey('Order',on_delete=models.SET_DEFAULT,default=1)
+    product=models.ForeignKey(Product,on_delete=models.SET_DEFAULT,default=1)
     quantity=models.IntegerField(null=True,blank=True,default=1)
     purchase_amount=models.DecimalField(decimal_places=2,max_digits=8)
 

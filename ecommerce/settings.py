41fecf32cf3a7430f25e49505b5acfc06ce7a8ff
prajjwalz1 <acharyaprajjwol152@gmail.com>
2025-01-19 22:38:08 +0545

    update compose file

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -234,6 +234,5 @@ Q_CLUSTER = {
 }
 
 
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_TASK_SERIALIZER = "json"
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
